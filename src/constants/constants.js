31bb5787463263f6ddca571ba567981925f8aaa3
Ricardo Rivera <glassboxwebmaster@gmail.com>
2023-03-14 00:59:04 -0700

    removed work in progress

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,15 +1,5 @@
 export const projects = [
   {
-    title: "New Portfolio (Work in Progress)",
-    description:
-      "This is a new portfolio, made completly from scratch. All designs and implemetations are all %100 original. I decide to work on the desktop version before to mobile version in order to full experiement with both the Tilt and Anime JavaScript libraries.",
-    image: "/images/portfolio2.png",
-    tags: ["HTML5", "CSS3", "JavaScript", "Tilt.js", "Anime.js"],
-    source: "https://newportfoliowip.netlify.app/",
-    visit: "https://github.com/Ricardo-Rivera-SB/NewPortfolioWIP",
-    id: 0,
-  },
-  {
     title: "Firewatch Homepage Clone",
     description:
       "This is a clone of the Firewatch homepage. My main goal with this project was to figure out how the parallax effect was done. I've made parallax effects before, but never anything with depth like this.",
